79c1ca0ad2a0674b5c67fe0ddd2132216cf35a6d
Mai Cáceres <93089029+maicaceresg@users.noreply.github.com>
2025-02-12 20:23:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const miPerfil = { 
+    nombre: "Maira",
+    apellido: "Cáceres",
+    edad: 27,
+    pais: "Argentina",  
+    profesion: "Acompañante Terapéutico",   
+    sabeManejar: false,
+    mascotas: ["Itachi"],
+    hobbies: ["Leer", "Escuchar música", "Ver K-dramas", "Jugar videojuegos", "Hacer deporte"],
+    tocaInstrumentos: false,
+    nivelDeIngles: "Intermedio",
+    ciudad: "Misiones",
+    redesSociales: {
+        instagram: "@Maicaceresg",
+        facebook: "Maira Cáceres",
+        twitter: "@Maicaceresg"
+    },
+    peliculasFavoritas: ["Me Before You", "El Diablo Viste a la Moda", "Titanic"],
+    comidasFavoritas: ["Milanesa con puré", "Pizza", "Empanadas", "Asado", "Hamburguesa"],
+    sabeCocinar: true,
+    diaDeLaSemanaFavorito: "Viernes",
+    objetivosPorCumplir: ["Terminar la carrera", "Viajar a Corea del Sur"]
+};
+
+console.log(miPerfil);
